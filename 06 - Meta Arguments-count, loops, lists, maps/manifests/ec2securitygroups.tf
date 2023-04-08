8bda3bbb5db6aa7c6ffa468dadf954da2236f3b8
oladaolu <107776620+oladaolu@users.noreply.github.com>
2023-04-07 20:07:02 -0400

    Update ec2securitygroups.tf

--- 06 - Meta Arguments-count, loops, lists, maps/manifests/ec2securitygroups.tf
+++ 06 - Meta Arguments-count, loops, lists, maps/manifests/ec2securitygroups.tf
@@ -1,56 +1,25 @@
-# Create Security Group - SSH Traffic
-resource "aws_security_group" "vpc-ssh" {
-  name        = "vpc-ssh"
-  description = "Dev VPC SSH"
-  ingress {
-    description = "Allow Port 22"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group" "web-traffic" {
+    name = "Allow HTTPS"
 
-  egress {
-    description = "Allow all ip and ports outbound"    
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    dynamic "ingress" {
+        iterator = port
+        for_each = var.ingressrules
+        content {
+        from_port = port.value
+        to_port = port.value
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+    }
 
-  tags = {
-    Name = "vpc-ssh"
-  }
+    dynamic "egress" {
+        iterator = port
+        for_each = var.egressrules
+        content {
+        from_port = port.value
+        to_port = port.value
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+    }
 }
-
-# Create Security Group - Web Traffic
-resource "aws_security_group" "vpc-web" {
-  name        = "vpc-web"
-  description = "Dev VPC Web"
-  ingress {
-    description = "Allow Port 80"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "Allow Port 443"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }  
-  egress {
-    description = "Allow all ip and ports outbound"    
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "vpc-web"
-  }
-}
-
